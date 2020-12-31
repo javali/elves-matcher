e468840a3d0e62fe0be3b09a232b2060f051e265
Jan van Lil <jan.vanlil@yahoo.de>
2020-12-31 13:17:15 +0100

    cleanup and tests

--- src/main/java/com/javali/gleif/elvesmatcher/ElvesMatcherRestApplication.java
+++ src/main/java/com/javali/gleif/elvesmatcher/ElvesMatcherRestApplication.java
@@ -1,6 +1,7 @@
 package com.javali.gleif.elvesmatcher;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Collections;
--- src/test/java/com/javali/gleif/elvesmatcher/ConverterIntegrationTest.java
+++ src/test/java/com/javali/gleif/elvesmatcher/ConverterIntegrationTest.java
@@ -1,91 +1,102 @@
 package com.javali.gleif.elvesmatcher;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.javali.gleif.elvesmatcher.model.ELF;
+import com.javali.gleif.elvesmatcher.controller.ConverterController;
 import com.javali.gleif.elvesmatcher.model.LegalForm;
-import jdk.internal.org.objectweb.asm.Handle;
-import org.junit.jupiter.api.Assertions;
+import com.javali.gleif.elvesmatcher.util.Converter;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-@SpringBootTest(
-    classes = ElvesMatcherRestApplication.class,
-    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ConverterController.class)
 class ConverterIntegrationTest {
 
-  @LocalServerPort private int port;
-  @Autowired private TestRestTemplate restTemplate;
-  private Map<String, List<String>> testObjects = createMap();
+	@Autowired
+	private WebApplicationContext context;
 
-  @Test
-  void testRunWithMultipleRequests() {
+	@MockBean
+	private Converter converter;
 
-    testObjects
-        .keySet()
-        .forEach(
-            legislation -> {
-              testObjects
-                  .get(legislation)
-                  .forEach(
-                      abbreviation -> {
-                        LegalForm legalForm = new LegalForm();
-                        legalForm.setLegislation(legislation);
-                        legalForm.setAbbreviation(abbreviation);
+	protected MockMvc mockMvc;
 
-                        String url = "http://localhost:" + port + "/convert";
-                        ELF elf = this.restTemplate.postForObject(url, legalForm, ELF.class);
+	private static ObjectMapper objectMapper = new ObjectMapper();
 
-                        Assertions.assertNotNull(elf);
-                      });
-            });
-  }
+	@BeforeEach
+	public void setup() {
+		this.mockMvc = MockMvcBuilders
+				.webAppContextSetup(this.context)
+				.build();
+	}
 
-  private Map<String, List<String>> createMap() {
-    Map<String, List<String>> map = new HashMap<>();
-    map.put("DE", new ArrayList<>());
-    map.get("DE").add("VVaG");
-    map.get("DE").add("GmbH");
-    map.get("DE").add("PartG mbB");
-    map.get("DE").add("OHG");
-    map.get("DE").add("OHG mbH");
-    map.get("DE").add("GmbH & Co. OHG");
-    map.get("DE").add("AG & Co. OHG");
-    map.get("DE").add("GmbH UG");
-    map.get("DE").add("UG (haftungsbeschränkt)");
-    map.get("DE").add("AG");
-    map.get("DE").add("PartG");
-    map.get("DE").add("KG");
-    map.get("DE").add("GmbH & Co. KG");
-    map.get("DE").add("UG (haftungsbeschränkt) & Co. KG AG & Co. KG");
-    map.get("DE").add("KGaA & Co. KG");
-    map.get("DE").add("Stiftung & Co. KG");
-    map.get("DE").add("REIT-AG");
-    map.get("DE").add("eG");
-    map.get("DE").add("GbR");
-    map.get("DE").add("InvAG");
-    map.get("DE").add("EWIV");
-    map.get("DE").add("e. K.");
-    map.get("DE").add("eK");
-    map.get("DE").add("e. Kfm.");
-    map.get("DE").add("e. Kffr");
-    map.get("DE").add("e.V.");
-    map.get("DE").add("eV");
-    map.get("DE").add("gGmbH");
-    map.get("DE").add("SE");
-    map.get("DE").add("SCE");
-    map.get("DE").add("KGaA");
-    map.get("DE").add("GmbH & Co. KGaA");
-    map.get("DE").add("AG & Co. KGaA");
-    map.get("DE").add("Stiftung & Co. KGaA");
-    map.get("DE").add("eG");
-    map.get("DE").add("e.G.");
-    map.get("DE").add("gAG");
-    return map;
-  }
+	@Test
+	void testControllerResponds() throws Exception {
+
+		LegalForm legalForm = new LegalForm();
+		legalForm.setLegislation("DE");
+		legalForm.setAbbreviation("GmbH");
+		this.mockMvc
+				.perform(post("/convert")
+						.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+						.content(objectMapper.writeValueAsString(legalForm)))
+				.andExpect(status().isOk());
+
+	}
+
+
+	private Map<String, List<String>> createMap() {
+		Map<String, List<String>> map = new HashMap<>();
+		map.put("DE", new ArrayList<>());
+		map.get("DE").add("VVaG");
+		map.get("DE").add("GmbH");
+		map.get("DE").add("PartG mbB");
+		map.get("DE").add("OHG");
+		map.get("DE").add("OHG mbH");
+		map.get("DE").add("GmbH & Co. OHG");
+		map.get("DE").add("AG & Co. OHG");
+		map.get("DE").add("GmbH UG");
+		map.get("DE").add("UG (haftungsbeschränkt)");
+		map.get("DE").add("AG");
+		map.get("DE").add("PartG");
+		map.get("DE").add("KG");
+		map.get("DE").add("GmbH & Co. KG");
+		map.get("DE").add("UG (haftungsbeschränkt) & Co. KG AG & Co. KG");
+		map.get("DE").add("KGaA & Co. KG");
+		map.get("DE").add("Stiftung & Co. KG");
+		map.get("DE").add("REIT-AG");
+		map.get("DE").add("eG");
+		map.get("DE").add("GbR");
+		map.get("DE").add("InvAG");
+		map.get("DE").add("EWIV");
+		map.get("DE").add("e. K.");
+		map.get("DE").add("eK");
+		map.get("DE").add("e. Kfm.");
+		map.get("DE").add("e. Kffr");
+		map.get("DE").add("e.V.");
+		map.get("DE").add("eV");
+		map.get("DE").add("gGmbH");
+		map.get("DE").add("SE");
+		map.get("DE").add("SCE");
+		map.get("DE").add("KGaA");
+		map.get("DE").add("GmbH & Co. KGaA");
+		map.get("DE").add("AG & Co. KGaA");
+		map.get("DE").add("Stiftung & Co. KGaA");
+		map.get("DE").add("eG");
+		map.get("DE").add("e.G.");
+		map.get("DE").add("gAG");
+		return map;
+	}
 }
